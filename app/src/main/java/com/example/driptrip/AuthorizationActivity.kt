c1d62e5ee9107b0e0dddce3e97821cf06730ad93
AntonPiekhotin <pehotin.anton@gmail.com>
2023-11-11 21:13:01 +0200

    added some style to reg and auth pages

--- app/src/main/java/com/example/driptrip/AuthorizationActivity.kt
+++ app/src/main/java/com/example/driptrip/AuthorizationActivity.kt
@@ -4,12 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import androidx.appcompat.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
 class AuthorizationActivity : AppCompatActivity() {
 
-    private lateinit var etEmail: EditText
-    private lateinit var etPassword: EditText
+    private lateinit var etEmail: AppCompatEditText
+    private lateinit var etPassword: AppCompatEditText
+    private lateinit var btnToRegistration: AppCompatTextView
+    private lateinit var btnLogin: AppCompatButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,17 +20,19 @@ class AuthorizationActivity : AppCompatActivity() {
 
         etEmail = findViewById(R.id.et_email)
         etPassword = findViewById(R.id.et_password)
+        btnToRegistration = findViewById(R.id.to_registration)
+        btnLogin = findViewById(R.id.btn_login)
 
-        findViewById<Button>(R.id.btn_login).setOnClickListener {
+
+        btnLogin.setOnClickListener {
             val email = etEmail.text.toString()
             val password = etPassword.text.toString()
 
-            // TODO: Авторизувати користувача
+            //TODO: Авторизувати користувача
         }
-    }
 
-    fun onLoginClick(view: View) {
-        startActivity(Intent(this, RegistrationActivity::class.java))
-        finish()
+        btnToRegistration.setOnClickListener {
+            startActivity(Intent(this, RegistrationActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/driptrip/RegistrationActivity.kt
+++ app/src/main/java/com/example/driptrip/RegistrationActivity.kt
@@ -2,15 +2,20 @@ package com.example.driptrip
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.UnderlineSpan
 import android.view.View
 import android.widget.*
+import androidx.appcompat.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
 class RegistrationActivity : AppCompatActivity() {
 
-    private lateinit var etUsername: EditText
-    private lateinit var etEmail: EditText
-    private lateinit var etPassword: EditText
+    private lateinit var etUsername: AppCompatEditText
+    private lateinit var etEmail: AppCompatEditText
+    private lateinit var etPassword: AppCompatEditText
+    private lateinit var btnToAuthorization: AppCompatTextView
+    private lateinit var btnRegister: AppCompatButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,22 +24,27 @@ class RegistrationActivity : AppCompatActivity() {
         etUsername = findViewById(R.id.et_username)
         etEmail = findViewById(R.id.et_email)
         etPassword = findViewById(R.id.et_password)
+        btnRegister = findViewById(R.id.btn_register)
+        btnToAuthorization = findViewById(R.id.to_authorization)
 
-        findViewById<Button>(R.id.btn_register).setOnClickListener {
+//        val signIn = findViewById<TextView>(R.id.to_authorization)
+
+        val string = "Sign in"
+        val spannableString = SpannableString(string)
+        spannableString.setSpan(UnderlineSpan(), 0, spannableString.length, 0)
+        btnToAuthorization.text = spannableString.toString()
+
+        btnRegister.setOnClickListener {
             val username = etUsername.text.toString()
             val email = etEmail.text.toString()
             val password = etPassword.text.toString()
         }
 
-        // TODO: connect to django server (?)
+        btnToAuthorization.setOnClickListener {
+            startActivity(Intent(this, AuthorizationActivity::class.java))
+        }
 
+        //TODO: connect to django server (?)
 
     }
-
-    fun onRegisterClick(view: View) {
-        startActivity(Intent(this, AuthorizationActivity::class.java))
-        finish()
-    }
-
-
 }
